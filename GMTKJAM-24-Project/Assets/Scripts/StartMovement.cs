0d9949d1841cce6a486dab58933b9af09052bfb8
michaelsn22 <michael.newman@sjsu.edu>
2024-08-16 19:44:14 -0700

    Added cinemachine + physics update
    Improved movement based on a cinemachine camera now. Added super basic functioning collision detection. Bumping into anything that isnt the plane under you will grow your scale.

--- GMTKJAM-24-Project/Assets/Scripts/StartMovement.cs
+++ GMTKJAM-24-Project/Assets/Scripts/StartMovement.cs
@@ -5,42 +5,102 @@ using UnityEngine;
 
 public class StartMovement : MonoBehaviour
 {
-    private float horizontalVal;
-    private float verticalVal;
     private Rigidbody rb;
-    public float burstValue = 20f;
-    public bool isGrounded; // Variable to check if the character is on the ground
+    public bool isGrounded;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
 
+    public Transform cam;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public float speed = 6f;
+    public float burstValueUpwards = 20f;
+
     [SerializeField] private LayerMask groundMask;
+
+    private Vector3 movement;
+    private bool jumpRequested = false;
+
+    //scaling vars
+    public float scaleIncrease = 0.3f; // 30% increase
+    private Vector3 originalScale;
+    private bool hasScaled = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        originalScale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // Input detection
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movement = new Vector3(horizontal, 0f, vertical).normalized;
 
-        movePlayer();
-    }
-
-    private void movePlayer()
-    {
-        horizontalVal = Input.GetAxis("Horizontal");
-        verticalVal = Input.GetAxis("Vertical");
-
-       //slime moving?
+        // Jump input detection
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            Debug.Log("jumping");
-            rb.AddForce(transform.up * burstValue, ForceMode.Impulse);
+            jumpRequested = true;
         }
 
-        Vector3 movementVector = new Vector3(horizontalVal, 0f, verticalVal);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
 
-        rb.AddForce(movementVector * burstValue);
+    void FixedUpdate()
+    {
+        MovePlayer();
+        if (jumpRequested)
+        {
+            Jump();
+            jumpRequested = false;
+        }
+    }
+
+    void MovePlayer()
+    {
+        if (movement.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            
+            rb.velocity = new Vector3(moveDir.x * speed, rb.velocity.y, moveDir.z * speed);
+        }
+        else //DECIDE WHETHER TO KEEP MOMENTUM OR NOT!
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
+
+    void Jump()
+    {
+        Debug.Log("jumping");
+        rb.AddForce(transform.up * burstValueUpwards, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string collidedObjectName = "";
+        collidedObjectName = collision.gameObject.name;
+        Debug.Log("We collided with "+collision.gameObject.name);
+
+        if (collidedObjectName.Contains("Plane"))
+        {
+            return;
+        }
+
+        // Calculate the new scale
+        Vector3 newScale = gameObject.transform.localScale * (1f + scaleIncrease);
+        
+        // Apply the new scale
+        transform.localScale = newScale;
+        
+        // Mark as scaled to prevent multiple scales
+        hasScaled = true;
+
+        Debug.Log("Object scaled up by 30%");
     }
 }
